2eb634c2bf0b17251c5c45011952a2d990c4db7e
Kyle Waltz <kylewaltz@yahoo.com>
2014-12-10 08:16:33 -0700

    Update Manager.java

--- src/Manager.java
+++ src/Manager.java
@@ -1,1 +1,24 @@
+import java.util.*
 // class to represent a manager - inherits Emplyee
+public class Manager extends Employee
+{
+  private ArrayList<Employee> directReports = new ArrayList<Employee>();
+  private String department;
+  
+  public String Manager ()
+  {
+    return department;
+  }
+  public ArrayList<Employee> getDirectReports ()
+  {
+    return directReports;
+  }
+  public String toString ()
+  {
+    return "Manager :" + super.toString()
+  }
+  public void addDirectReport (Employee employee)
+  {
+    directReports.add(employee);
+  }
+}
