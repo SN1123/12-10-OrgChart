761d5e59ef49019cb3bacd91dfc59959ac891674
Kyle Waltz <kylewaltz@yahoo.com>
2014-12-15 08:14:29 -0700

    Update Manager.java

--- src/Manager.java
+++ src/Manager.java
@@ -1,71 +1,49 @@
-// class to represent an emplayee - inherits Person
-public class Employee extends Person
+import java.util.*;
+// class to represent a manager - inherits Emplyee
+public class Manager extends Employee
 {
-  private double hourlyWage;
-  private int hoursWorked;
-  private String jobTitle;
-  private Manager manager;
+  private ArrayList<Employee> directReports = new ArrayList<Employee>();
+  private String department;
   
-  public Employee (String firstName, String lastName, int age, double wagePerHour, int hours, String job)
+  public Manager (String firstName, String lastName, int age, double wagePerHour, int hours, String job, String Dept)
   {
-    super(firstName, lastName, age);
-    hoursWorked = hours;
-    hourlyWage = wagePerHour;
-    jobTitle = job;
+    super(firstName, lastName, age, wagePerHour, hours, job);
+    department = Dept;
   }
-  public double getWage ()
+  public String getDepartment ()
   {
-    return hourlyWage;
+    return department;
   }
-  public int getHours ()
+  public ArrayList<Employee> getDirectReports ()
   {
-    return hoursWorked;
-  }
-  public double getWeeklySalary ()
-  {
-    return hoursWorked*hourlyWage;
-  }
-  public double getYearlySalary ()
-  {
-    return getWeeklySalary() * 52;
-  }
-  public String getJobTitle ()
-  {
-    return jobTitle;
+    return directReports;
   }
   public String toString ()
   {
-    return super.toString() + " " + jobTitle + "@ " + getYearlySalary();
+    return "Manager :" + super.toString();
   }
-  public Manager getManager ()
+  public void addDirectReport (Employee employee)
   {
-    return manager;
+    directReports.add(employee);
   }
-  public void setWage (double newWage)
+  public void removeDirectReport (Employee employee)
   {
-    hourlyWage = newWage;
+    directReports.remove(employee);
   }
-  public void setHours (int h)
+  public void setDepartment (String dept)
   {
-    hoursWorked = h;
+    department = dept;
   }
-  public void setJobTitle (String title)
+  public boolean equals (Object obj)
   {
-    jobTitle = title;
-  }
-  public void giveRaise ()
-  {
-    hourlyWage++;
-  }
-  public void setManager (Manager newManager)
-  {
-    if (manager != null)
+    if (obj instanceof Manager)
     {
-      //remove employee from manager's list
-      manager.removeDirectReport(this);
+      Manager other = (Manager) obj;
+      return directReports == other.getDirectReports() && department == other.getDepartment();
     }
-    //add employee to new manager
-    manager = newManager;
-    manager.addDirectReport(this);
+    else
+    {
+      return false;
+    }
   }
 }
